6c8b15d5ba3bc1c29e3c039f99ebdee9e6a02765
leontari <262930388+leontari@users.noreply.github.com>
2026-04-05 21:53:25 +0300

    chore: use uv as the python's front deps manager via workspace concept (#13)

---
+++ packages-shared/python-packages/template-package/src/template_package/__main__.py
@@ -0,0 +1,4 @@
+print("print from: template-package: __main__.py")
+
+def foo():
+    print("print from: template-package: __main__.py: foo()")
---
+++ services/template-service/src/template_service/main.py
@@ -0,0 +1,10 @@
+from template_package import py_module
+
+
+def main():
+    print("Hello from template-service!")
+    py_module.module_function()
+
+
+if __name__ == "__main__":
+    main()
